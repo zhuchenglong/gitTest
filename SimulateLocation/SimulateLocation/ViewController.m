41988c20c8ffba00435cc879004293d0620cbae7
来宝 <460876003@qq.com>
2019-03-11 15:11:02 +0800

    第二次提交

--- SimulateLocation/SimulateLocation/ViewController.m
+++ SimulateLocation/SimulateLocation/ViewController.m
@@ -20,6 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSLog(@"222");
+    
     
     UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(30, 100, self.view.bounds.size.width-60, 150)];
     imageView.image = [UIImage imageNamed:@"tupian"];
